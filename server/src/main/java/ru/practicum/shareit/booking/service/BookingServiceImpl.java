2e74ed08bf0f46e65fc4e3aabebb7a74acc5a094
Barkov <venediktkaverin@gmail.com>
2026-05-16 20:08:31 +0300

    Исправить рассинхрон

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -133,7 +133,7 @@ public class BookingServiceImpl implements BookingService {
     @Override
     public boolean existsBookingByUserIdAndItemId(Long itemId, Long userId) {
         return bookingRepository.existsByItemIdAndBookerIdAndStatusAndEndBefore(
-                itemId, userId, BookingStatus.APPROVED, LocalDateTime.now().plusSeconds(5)
+                itemId, userId, BookingStatus.APPROVED, LocalDateTime.now().plusSeconds(1)
         );
     }
 
